1162cff661c4bb73145209ef17bb4825fd947b01
Đào Mai Chi <144459405+mai-chi03@users.noreply.github.com>
2024-05-21 08:27:06 +0700

    Add files via upload

---
+++ SQLite_QLSV/app/src/main/java/com/example/sqlite_qlsv/Cursor.java
@@ -0,0 +1,15 @@
+package com.example.sqlite_qlsv;
+
+public class Cursor {
+    public void moveToNext() {
+    }
+
+    public boolean isAfterLast() {
+    }
+
+    public String getString(int i) {
+    }
+
+    public void close() {
+    }
+}
---
+++ SQLite_QLSV/app/src/main/java/com/example/sqlite_qlsv/edtmalop.java
@@ -0,0 +1,4 @@
+package com.example.sqlite_qlsv;
+
+public class edtmalop {
+}
